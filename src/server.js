86b1fae0d7dc0759608c536bf78609124836f4b3
user <60804817+ichg4354@users.noreply.github.com>
2020-08-15 02:17:15 +0900

    add realtime chat on console

--- src/server.js
+++ src/server.js
@@ -1,12 +1,33 @@
 import Express from "express";
+import socketIO from "socket.io";
+import logger from "morgan";
 
-const app = Express();
 const port = 1001;
 
-app.get("/", (req, res) => res.render("home"));
+const app = Express();
 
+app.get("/", (req, res) => res.render("home"));
 app.set("view engine", "pug");
 app.set("views", "src/views");
 app.use(Express.static("src/static"));
+app.use(Express.static("node_modules"));
 
-app.listen(port, () => console.log("✅ listening to http://localhost:1001"));
+const handleListen = () => {
+  console.log("✅ listening to http://localhost:1001");
+};
+
+const server = app.listen(port, handleListen);
+
+const io = socketIO.listen(server);
+
+io.on("connection", (socket) => {
+  return socket.on(
+    "newMessage",
+    ({ message }) =>
+      socket.broadcast.emit("messageNotification", {
+        message,
+        nickname: socket.nickname || "BLANK USER",
+      }),
+    socket.on("setNickName", ({ nickname }) => (socket.nickname = nickname))
+  );
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,14 @@
-console.log("HI");
+const socket = io.connect("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message: message });
+}
+
+function setnickName(nickname) {
+  socket.emit("setNickName", { nickname });
+}
+
+socket.on("messageNotification", ({ message,nickname }) =>
+  console.log(`${nickname}:  ${message}`)
+);
+
