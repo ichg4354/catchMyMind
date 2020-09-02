0c350a7333b7fec3ad747b3035e111e15206f8bc
user <60804817+ichg4354@users.noreply.github.com>
2020-09-02 21:18:22 +0900

    add canvas hide function for not leader

--- assets/js/player.js
+++ assets/js/player.js
@@ -3,11 +3,11 @@ import { blockCanvasUse, enableCanvasUse } from "./canvas";
 export const handleGameStart = () => {
   console.log("game has started");
   blockCanvasUse();
-  // alert("game has started");
+  alert("game has started");
 };
 
 export const handleNotifyLeader = ({ word }) => {
   console.log(`you are the leader your word:${word}`);
   enableCanvasUse();
-  // alert(`you are the leader your word is ${word}`);
+  alert(`you are the leader your word is ${word}`);
 };
