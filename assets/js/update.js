79c3d82d3eba503bcbca66941267748e98e8ef30
user <60804817+ichg4354@users.noreply.github.com>
2020-09-06 15:58:32 +0900

    kluge for double login error

--- assets/js/update.js
+++ assets/js/update.js
@@ -3,15 +3,13 @@ const board = document.getElementById("jsBoard");
 export const handleUpdate = ({ sockets }) => {
   resetBoard();
   let beforeNickName = null;
-  //cludge
+  //kluge for double login when loggin through nickName fisrt set
   sockets.forEach((socket) => {
     if (socket.nickName != beforeNickName) {
       let boardElement = document.createElement("div");
       boardElement.innerText = `${socket.nickName}:${socket.points}`;
       board.appendChild(boardElement);
       beforeNickName = socket.nickName;
-    } else {
-      console.log("FUCK YEA");
     }
   });
 };
